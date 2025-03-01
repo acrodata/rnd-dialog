62c6a23c61bd4cab9800e347e960ea4f8d5f1bc5
nzbin <nzb329@163.com>
2025-03-01 21:56:16 +0800

    refactor(dev-app): add dialog config form

--- projects/dev-app/src/app/app.component.ts
+++ projects/dev-app/src/app/app.component.ts
@@ -1,19 +1,32 @@
 import { RndDialog, RndDialogDragHandle } from '@acrodata/rnd-dialog';
+import { Dialog, DialogConfig, DialogRef } from '@angular/cdk/dialog';
 import { Component, inject, TemplateRef } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RndDialogDragHandle],
+  imports: [FormsModule, RndDialogDragHandle],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
+  private dialog = inject(Dialog);
   private rndDialog = inject(RndDialog);
 
+  config: DialogConfig<string, DialogRef> = {
+    data: 'Hello, World!',
+    width: '600px',
+    height: '600px',
+    minWidth: '300px',
+    minHeight: '300px',
+    maxWidth: '800px',
+    maxHeight: '800px',
+    hasBackdrop: true,
+    disableClose: false,
+  };
+
   openDialog(tpl: TemplateRef<any>) {
-    this.rndDialog.open(tpl, {
-      data: 'Hello, World!',
-    });
+    this.rndDialog.open(tpl, this.config);
   }
 }
