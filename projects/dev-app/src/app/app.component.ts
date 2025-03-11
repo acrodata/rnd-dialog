2eaa564cf796b5d9c18d8e17e724a1b0a3ffbe07
nzbin <nzb329@163.com>
2025-03-11 19:55:32 +0800

    refactor(dev-app): add gui to improve configs

--- projects/dev-app/src/app/app.component.ts
+++ projects/dev-app/src/app/app.component.ts
@@ -1,12 +1,14 @@
+import { GuiFields, GuiModule } from '@acrodata/gui';
 import { RndDialog, RndDialogDragHandle } from '@acrodata/rnd-dialog';
 import { Dialog, DialogConfig, DialogRef } from '@angular/cdk/dialog';
 import { Component, inject, TemplateRef } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [FormsModule, RndDialogDragHandle],
+  imports: [FormsModule, RndDialogDragHandle, GuiModule, MatButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
@@ -14,7 +16,46 @@ export class AppComponent {
   private dialog = inject(Dialog);
   private rndDialog = inject(RndDialog);
 
-  config: DialogConfig<string, DialogRef> = {
+  guiConfig: GuiFields = {
+    data: {
+      name: 'data',
+      type: 'textarea',
+    },
+    width: {
+      name: 'width',
+      type: 'text',
+    },
+    height: {
+      name: 'height',
+      type: 'text',
+    },
+    minWidth: {
+      name: 'minWidth',
+      type: 'text',
+    },
+    minHeight: {
+      name: 'minHeight',
+      type: 'text',
+    },
+    maxWidth: {
+      name: 'maxWidth',
+      type: 'text',
+    },
+    maxHeight: {
+      name: 'maxHeight',
+      type: 'text',
+    },
+    hasBackdrop: {
+      name: 'hasBackdrop',
+      type: 'switch',
+    },
+    disableClose: {
+      name: 'disableClose',
+      type: 'switch',
+    },
+  };
+
+  guiModel: DialogConfig<string, DialogRef> = {
     data: 'Hello, World!',
     width: '400px',
     height: '400px',
@@ -27,6 +68,6 @@ export class AppComponent {
   };
 
   openDialog(tpl: TemplateRef<any>) {
-    this.rndDialog.open(tpl, this.config);
+    this.rndDialog.open(tpl, this.guiModel);
   }
 }
