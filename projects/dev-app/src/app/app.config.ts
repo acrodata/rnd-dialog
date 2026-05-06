54d654927e4418d13105500f7d5ac1621d027430
nzbin <nzb329@163.com>
2026-05-06 21:07:11 +0800

    refactor(dev-app): disable popover option for overlay config

--- projects/dev-app/src/app/app.config.ts
+++ projects/dev-app/src/app/app.config.ts
@@ -1,7 +1,17 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
+import { OVERLAY_DEFAULT_CONFIG } from '@angular/cdk/overlay';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)],
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    {
+      provide: OVERLAY_DEFAULT_CONFIG,
+      useValue: {
+        usePopover: false,
+      },
+    },
+  ],
 };
